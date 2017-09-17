8b29a7526cab95511282cbcaa24324442143b48d
dhruv007patel <dhruvnpatel1997@gmail.com>
2017-09-18 00:34:23 +0530

    C++

---
+++ LIKECS02.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=1;n>=i;i++)
+        cout<<i+(n/2)<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
